462f146b3334df8b89a9ce37c3e467177c549a78
Mélany Condé <melany.conde@gmail.com>
2021-03-11 12:34:58 +0100

    Formulaire

--- JS/shopping-cart.js
+++ JS/shopping-cart.js
@@ -51,8 +51,97 @@ function onLoadCartNumbers() {
 }
 
 
+function displayForm () {
+
+    const itemElement = document.createElement('div')  ;
+    itemElement.setAttribute('class','item-form');
+
+
+    const itemNameElement = document.createElement('div')
+    const itemLabelNameElement = document.createElement('label');
+    const itemInputNameElement = document.createElement('input')
+    itemNameElement.setAttribute('class','item-form-name');
+    itemLabelNameElement.textContent = "Nom",
+    itemInputNameElement.type = "text",
+    itemInputNameElement.placeholder = "Entrez votre nom",
+    itemNameElement.appendChild(itemLabelNameElement);
+    itemNameElement.appendChild(itemInputNameElement);
+
+
+    const itemSurnameElement = document.createElement('div')
+    const itemLabelSurnameElement = document.createElement('label');
+    const itemInputSurnameElement = document.createElement('input')
+    itemSurnameElement.setAttribute('class','item-form-surname');
+    itemLabelSurnameElement.textContent = "Prénom",
+    itemInputSurnameElement.type = "text",
+    itemInputSurnameElement.placeholder = "Entrez votre prénom",
+    itemSurnameElement.appendChild(itemLabelSurnameElement);
+    itemSurnameElement.appendChild(itemInputSurnameElement);
+
+    const itemAdressElement = document.createElement('div')
+    const itemLabelAdressElement = document.createElement('label');
+    const itemInputAdressElement = document.createElement('input')
+    itemAdressElement.setAttribute('class','item-form-Adress');
+    itemLabelAdressElement.textContent = "Adresse",
+    itemInputAdressElement.type = "text",
+    itemInputAdressElement.placeholder = "Entrez votre adresse",
+    itemAdressElement.appendChild(itemLabelAdressElement);
+    itemAdressElement.appendChild(itemInputAdressElement);
+
+    const itemCityElement = document.createElement('div')
+    const itemLabelCityElement = document.createElement('label');
+    const itemInputCityElement = document.createElement('input')
+    itemCityElement.setAttribute('class','item-form-City');
+    itemLabelCityElement.textContent = "Ville",
+    itemInputCityElement.type = "text",
+    itemInputCityElement.placeholder = "Entrer votre nom",
+    itemCityElement.appendChild(itemLabelCityElement);
+    itemCityElement.appendChild(itemInputCityElement);
+
+    const itemPostalElement = document.createElement('div')
+    const itemLabelPostalElement = document.createElement('label');
+    const itemInputPostalElement = document.createElement('input')
+    itemPostalElement.setAttribute('class','item-form-postal');
+    itemLabelPostalElement.textContent = "Code Postal",
+    itemInputPostalElement.type = "number",
+    itemInputPostalElement.placeholder = "Entrez votre code postal",
+    itemPostalElement.appendChild(itemLabelPostalElement);
+    itemPostalElement.appendChild(itemInputPostalElement);
+
+    const itemMailElement = document.createElement('div')
+    const itemLabelMailElement = document.createElement('label');
+    const itemInputMailElement = document.createElement('input')
+    itemMailElement.setAttribute('class','item-form-mail');
+    itemLabelMailElement.textContent = "Adresse Mail",
+    itemInputMailElement.type = "text",
+    itemInputMailElement.placeholder = "Entrez votre e-mail",
+    itemMailElement.appendChild(itemLabelMailElement);
+    itemMailElement.appendChild(itemInputMailElement);
+
+    const itemButton = document.createElement ('button')
+    itemButton.setAttribute ('class','button-form')
+    itemButton.textContent = "Valider";
+    
+
+
+    itemElement.appendChild(itemNameElement);
+    itemElement.appendChild(itemSurnameElement);
+    itemElement.appendChild(itemAdressElement);
+    itemElement.appendChild(itemCityElement);
+    itemElement.appendChild(itemPostalElement);
+    itemElement.appendChild(itemMailElement);
+    itemElement.appendChild(itemButton)
+
+
+
+    document.getElementById('form-container').appendChild(itemElement);
+} 
+
+
+
 async function init() {
     displayCart();
     onLoadCartNumbers();
+    displayForm()
 }
 init();
