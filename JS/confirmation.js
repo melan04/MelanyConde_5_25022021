871c61b591cbf6a55604621ecda15ceb8c19d39e
Mélany Condé <melany.conde@gmail.com>
2021-03-22 18:30:34 +0100

    Projet terminé

--- JS/confirmation.js
+++ JS/confirmation.js
@@ -9,8 +9,8 @@ total = JSON.parse(total);
 const order = document.getElementById("order_confirmed");
 order.innerHTML = "";
 order.innerHTML += `
-<h2>Merci pour votre commande</h2>
-    <h3>Total du panier: ${total} €</h3>
+<h2>Merci pour votre commande ! </h2>
+    <h3>Montant total de votre commande: ${total} €</h3>
     <h3>Numéro de la commande: </br> ${orderId}</h3>`;
 
 // Afficher le nombre de produits dans le panier dans le header
